9fbe0a3adbb27841c056712e14de70494888f768
jamesmai0512 <jamesmai.sg@gmail.com>
2020-03-28 22:57:38 +0800

    define PUT router for toggle

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks, :collection => { :delete => :put }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # /tasks/1/toggle
+  put "tasks/:id/toggle", to: "tasks#toggle", as: "toggle"
 end
