405b0c2a04be93ea51a20dcb6c5c5ca5ad0ac3d2
jamesmai0512 <jamesmai.sg@gmail.com>
2020-03-29 21:13:24 +0800

    jquery change and get status (checked, uncheck) of checkbox in application.js

--- todo/app/assets/javascripts/application.js
+++ todo/app/assets/javascripts/application.js
@@ -14,3 +14,29 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+// Use ready() to make a function available after the document is loaded:
+// https://www.w3schools.com/jquery/event_ready.asp
+$(document).ready(function(){
+    // console.log("hello");
+    // console.log($(".completed-check"));
+
+    // The change event occurs when the value of an element has been changed (only works on <input>,
+    // <textarea> and <select> elements).
+    // https://www.w3schools.com/jquery/event_change.asp
+    $(".completed-check").change(function() {
+        // TODO: Research about this in Javascript
+
+        // The val() method returns or sets the value attribute of the selected elements.
+        // https://www.w3schools.com/jquery/html_val.asp
+
+        // is(): This method is also very simple and easy to use.
+        // By using this we can easily find whether a checked box is checked or not.
+        // https://www.geeksforgeeks.org/how-to-check-whether-a-checkbox-is-checked-in-jquery/
+        // this --> is input type checkbox element
+        // $(this).is(":checked")
+        console.log($(this).is(":checked"))
+    });
+
+});
